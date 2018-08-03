a42d7e4759dde43bf2a0337b2769fa11d4e73e37
smth-0 <danil.cherkasov@mail.ru>
2018-08-03 14:34:00 +0600

    Initial commit

---
+++ app/src/main/java/com/example/lunar/a2048/StartMenuActivity.java
@@ -0,0 +1,13 @@
+package com.example.lunar.a2048;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartMenuActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_menu);
+    }
+}
